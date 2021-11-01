cfdd6eb5949b0a40c6fce085f19029870accbb92
Jacky Wang <jacky.wang.ay@gmail.com>
2021-11-01 15:33:10 -0400

    added unity files

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector2 newVelocity = new Vector2(horiz, vert);
+        GetComponent<Rigidbody2D>().velocity = newVelocity * moveSpeed;
+
+            animator.SetFloat("Horizontal", horiz);
+        animator.SetFloat("Vertical", vert);
+        
+        animator.SetFloat("Speed", newVelocity.sqrMagnitude);
+
+    }
+}
