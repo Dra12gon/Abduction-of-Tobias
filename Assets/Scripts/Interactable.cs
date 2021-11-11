f817884277b2754740499961643d9030b9d7c54b
Ming Jie Wang <93546793+Dra12gon@users.noreply.github.com>
2021-11-11 04:06:21 -0500

    Made mc untied animation + e button popup from interactive objects

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    public GameObject EPopup;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            print("Player now in range");
+            EPopup.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            print("Player now not in range");
+            EPopup.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed;
-
+    public GameObject player;
     public Rigidbody2D rb;
     public Animator animator;
+    public GameObject crate;
+    public bool hasKey;
+    public bool isFree;
 
     // Update is called once per frame
     void Update()
@@ -19,10 +22,13 @@ public class PlayerMovement : MonoBehaviour
         Vector2 newVelocity = new Vector2(horiz, vert);
         GetComponent<Rigidbody2D>().velocity = newVelocity * moveSpeed;
 
-            animator.SetFloat("Horizontal", horiz);
+        animator.SetFloat("Horizontal", horiz);
         animator.SetFloat("Vertical", vert);
         
         animator.SetFloat("Speed", newVelocity.sqrMagnitude);
+        animator.SetBool("isFree", isFree); 
+
+        
 
     }
 }
