5961fff1a67bc0eb9cb996de8db85e92e89c7ecc
samgib03 <82425110+samgib03@users.noreply.github.com>
2021-12-08 04:10:30 -0500

    added sounds
    for the music I just picked whatever sounded decent. change it if you like

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    public bool isMute = false;
+    public Sprite Muted;
+    public Sprite Unmute;
+    public Button mutebutt;
+    public AudioSource inside;
+    public AudioSource outside;
+    public void Mute()
+    {
+        if (isMute == false)
+        {
+            mutebutt.image.sprite = Muted;
+            isMute = true;
+            inside.volume = 0;
+            outside.volume = 0;
+        } else if (isMute == true)
+        {
+            mutebutt.image.sprite = Unmute;
+            isMute = false;
+            inside.volume = 1;
+            outside.volume = 1;
+        }
+    }
+}
